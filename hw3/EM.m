f1d9cc1df1f6a1e249b75deb4346c62fa837e258
Matt Dickenson <mcdickenson@gmail.com>
2014-10-26 19:49:03 -0400

    clean up code for hw 3 problem 2

--- hw3/EM.m
+++ hw3/EM.m
@@ -1,21 +1,14 @@
 function [lambda, M, Sigma, R] = EM(Z, M, sigma2)
-  % dim(Z) = d x I
-  % dim(M) = d x K
-  % size(lambda) = K
-  
-  % size(Sigma) = d x d x K
-  
-  d = size(Z, 1)
-  I = size(Z, 2)
+  [d, I] = size(Z)
   K = size(M, 2)
   
   % Initialize uninformative prior
   lambda = repmat(1/K, K, 1)
-  mu = M
   Sigma = zeros(d, d, K)
   for k=1:K
     Sigma(:, :, k) = sigma2(k)*eye(d)
   end
+  R = zeros(K, I)
   
   % initialize f(theta) values for iteration
   f_p = intmax;
@@ -23,27 +16,21 @@ function [lambda, M, Sigma, R] = EM(Z, M, sigma2)
     
   while f_p > f_c + sqrt(eps)
     % E-step
-    R = E(lambda, mu, Sigma, Z)
+    for k=1:K
+      R(k, :) = lambda(k) * mvnpdf(Z', M(:, k)', Sigma(:, :, k))
+    end
+    R = R ./ (ones(K, 1) * sum(R));
     
     % M-step
     for k=1:K
-      lambda(k) = mean(R(k, :))
-      mu(:, k) = sum( (ones(d, 1) *R(k, :)) .* Z, 2 )  / sum(R(k, :))
-      diff = Z - (mu(:, k) * ones(1, I))
-      Sigma(:, :, k) = (((ones(d, 1) *R(k, :)) .* diff) * diff') / sum(R(k, :))
+      lambda(k) = mean(R(k, :));
+      r_d = ones(d, 1) *R(k, :);
+      M(:, k) = sum( r_d .* Z, 2 )  / sum(R(k, :));
+      diff = Z - (M(:, k) * ones(1, I));
+      Sigma(:, :, k) = ((r_d .* diff) * diff') / sum(R(k, :));
     end
+    
     f_p = f_c;
-    f_c = -sum(log(sum(R, 2)))
+    f_c = -sum(log(sum(R, 2)));
   end
-  M = mu
-end
-
-function [R] = E(lambda, mu, Sigma, Z)
-  K = size(mu, 2)
-  I = size(Z, 2)
-  R = zeros(K, I)
-  for k=1:K
-    R(k, :) = lambda(k) * mvnpdf(transpose(Z), transpose(mu(:, k)), Sigma(:, :, k))
-  end
-  R = R ./ (ones(K, 1) * sum(R))
 end
