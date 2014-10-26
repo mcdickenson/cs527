a4cd65baaee7db72f4a144387c6ac8272c39d12b
Matt Dickenson <mcdickenson@gmail.com>
2014-10-26 19:43:19 -0400

    implement hw 3 problem 2

--- hw3/EM.m
+++ hw3/EM.m
@@ -24,32 +24,16 @@ function [lambda, M, Sigma, R] = EM(Z, M, sigma2)
   while f_p > f_c + sqrt(eps)
     % E-step
     R = E(lambda, mu, Sigma, Z)
+    
     % M-step
     for k=1:K
       lambda(k) = mean(R(k, :))
-      mu(:, k) = sum(sum( (ones(d, 1) *R(k, :)) .* Z ) ) / sum(R(k, :))
+      mu(:, k) = sum( (ones(d, 1) *R(k, :)) .* Z, 2 )  / sum(R(k, :))
       diff = Z - (mu(:, k) * ones(1, I))
-      tmp = (ones(d, 1) *R(k, :))
-% %       size(diff)
-% %       size(tmp)
-% %       return
-%       tmp2 = sum(sum( tmp * (diff' * diff) ) / sum(R(k, :)))
-%       size(tmp2)
-%       size(sum(R(k, :)))
-%       return
-%       size(R(k, :))
-%       tmp3 = sum( tmp .* (diff .^2)) / sum(R(k, :))
-      s_k = 0
-      for i = 1:I
-        s_k = s_k + R(k, i) * (Z(:, i) - mu(:, k)) * transpose(Z(:, i) - mu(:, k))
-      end
-      Sigma(:, :, k) = s_k / sum(R(k, :))
+      Sigma(:, :, k) = (((ones(d, 1) *R(k, :)) .* diff) * diff') / sum(R(k, :))
     end
-%     R = closest_cluster(Z, M);
-%     M = (Z * transpose(R)) ./ (transpose(sum(R, 2) * ones(1, 2)));
     f_p = f_c;
-    neg_log_like = -sum(log(sum(R, 2)))
-    f_c = neg_log_like
+    f_c = -sum(log(sum(R, 2)))
   end
   M = mu
 end
@@ -59,13 +43,7 @@ function [R] = E(lambda, mu, Sigma, Z)
   I = size(Z, 2)
   R = zeros(K, I)
   for k=1:K
-%     for i=1:I
-%       R(k, i) = lambda(k) * mvnpdf(Z(: , i), 
-%     end
     R(k, :) = lambda(k) * mvnpdf(transpose(Z), transpose(mu(:, k)), Sigma(:, :, k))
   end
   R = R ./ (ones(K, 1) * sum(R))
 end
-
-% function [f] = neg_log_like(lambda, mu, Sigma, Z)
-% end
