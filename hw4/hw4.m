93532b35b58c6c78b9d603be7da9771df6c36f87
Matt Dickenson <mcdickenson@gmail.com>
2014-11-18 17:24:49 -0700

    complete hw4 1b

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -18,25 +18,49 @@ sigma = 3
 img = imread('/Users/mcdickenson/github/cs527/hw4/shadow.jpg');
 R = -4:4
 zs = zeros(size(R, 2), size(R, 2))
-disp1 = struct('vals', { zs zs })
-disp2 = struct('vals', { zs zs })
-disp3 = struct('vals', { zs zs })
+disp1 = zs
+disp2 = zs
+disp3 = zs
 offset = ceil(size(R, 2)/2)
 for d1=R
   for d2=R
-    tmp1 = dissimilarity(img, X(:, 1), X(:, 1)+[d1 d2]', sigma);
-    tmp2 = dissimilarity(img, X(:, 2), X(:, 2)+[d1 d2]', sigma);
-    tmp3 = dissimilarity(img, X(:, 3), X(:, 3)+[d1 d2]', sigma);
-    for i = 1:2
-      disp1(i).vals(d1+offset, d2+offset) = tmp1(i);
-      disp2(i).vals(d1+offset, d2+offset) = tmp2(i);
-      disp3(i).vals(d1+offset, d2+offset) = tmp3(i);
-    end
+    disp1(d1+offset, d2+offset) = dissimilarity(img, X(:, 1), X(:, 1)+[d1 d2]', sigma);
+    disp2(d1+offset, d2+offset) = dissimilarity(img, X(:, 2), X(:, 2)+[d1 d2]', sigma);
+    disp3(d1+offset, d2+offset) = dissimilarity(img, X(:, 3), X(:, 3)+[d1 d2]', sigma);
   end
 end
 
 dissimilarity(img, X(:, 1), X(:, 1), sigma)
 dissimilarity(img, X(:, 1), X(:, 2), sigma)
 
-img(1, 1)
+mesh(disp1)
 
+figure1 = figure;
+axes1 = axes('Parent',figure1);
+view(axes1,[74.5 54]);
+grid(axes1,'on');
+hold(axes1,'all');
+mesh(disp1)
+xlabel({'d2'},'HorizontalAlignment','right');
+ylabel({'d1'},'HorizontalAlignment','left');
+title({'Dissimilarity for i=1'});
+
+figure1 = figure;
+axes1 = axes('Parent',figure1);
+view(axes1,[74.5 54]);
+grid(axes1,'on');
+hold(axes1,'all');
+mesh(disp2)
+xlabel({'d2'},'HorizontalAlignment','right');
+ylabel({'d1'},'HorizontalAlignment','left');
+title({'Dissimilarity for i=2'});
+
+figure1 = figure;
+axes1 = axes('Parent',figure1);
+view(axes1,[74.5 54]);
+grid(axes1,'on');
+hold(axes1,'all');
+mesh(disp3)
+xlabel({'d2'},'HorizontalAlignment','right');
+ylabel({'d1'},'HorizontalAlignment','left');
+title({'Dissimilarity for i=3'});
